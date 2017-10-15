6d6dcde63b0506ec67801e2410910c871c16a379
Luke McGregor <lukemcgregor@users.noreply.github.com>
2017-10-15 21:27:02 +1300

    trying localhost

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var request = require('request');
 setInterval(
     ()=>{
         console.log(`${new Date().toISOString()}`);
-        request('http://aci-demo-web/', function (error, response, body) {
+        request('http://localhost/', function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log(body);
              }
